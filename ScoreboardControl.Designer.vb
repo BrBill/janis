5f29b9cfbe626106659c93db4bb066de78764a47
Bill Cernansky <bill.cernansky@jeppesen.com>
2019-03-22 13:10:43 -0700

    Include Scoreboard control, as I clearly must have missed it before.

---
+++ ScoreboardControl.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ScoreboardControl
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTeamLocRight = New System.Windows.Forms.Label()
+        Me.lblTeamNameRight = New System.Windows.Forms.Label()
+        Me.lblTeamNameLeft = New System.Windows.Forms.Label()
+        Me.lblTeamLocLeft = New System.Windows.Forms.Label()
+        Me.lblScoreLeft = New System.Windows.Forms.Label()
+        Me.lblScoreRight = New System.Windows.Forms.Label()
+        Me.FadeTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblTeamLocRight
+        '
+        Me.lblTeamLocRight.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.lblTeamLocRight.BackColor = System.Drawing.Color.Maroon
+        Me.lblTeamLocRight.Font = New System.Drawing.Font("Roboto Slab", 33.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeamLocRight.ForeColor = System.Drawing.Color.White
+        Me.lblTeamLocRight.Location = New System.Drawing.Point(843, 53)
+        Me.lblTeamLocRight.Name = "lblTeamLocRight"
+        Me.lblTeamLocRight.Size = New System.Drawing.Size(412, 65)
+        Me.lblTeamLocRight.TabIndex = 0
+        Me.lblTeamLocRight.Text = "Right Team City" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblTeamLocRight.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        Me.lblTeamLocRight.UseCompatibleTextRendering = True
+        '
+        'lblTeamNameRight
+        '
+        Me.lblTeamNameRight.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblTeamNameRight.BackColor = System.Drawing.Color.Maroon
+        Me.lblTeamNameRight.Font = New System.Drawing.Font("Roboto Slab", 33.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeamNameRight.ForeColor = System.Drawing.Color.White
+        Me.lblTeamNameRight.Location = New System.Drawing.Point(843, 118)
+        Me.lblTeamNameRight.Name = "lblTeamNameRight"
+        Me.lblTeamNameRight.Size = New System.Drawing.Size(412, 65)
+        Me.lblTeamNameRight.TabIndex = 1
+        Me.lblTeamNameRight.Text = "Right Team Name"
+        Me.lblTeamNameRight.TextAlign = System.Drawing.ContentAlignment.TopRight
+        Me.lblTeamNameRight.UseCompatibleTextRendering = True
+        '
+        'lblTeamNameLeft
+        '
+        Me.lblTeamNameLeft.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblTeamNameLeft.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer))
+        Me.lblTeamNameLeft.Font = New System.Drawing.Font("Roboto Slab", 33.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeamNameLeft.ForeColor = System.Drawing.Color.White
+        Me.lblTeamNameLeft.Location = New System.Drawing.Point(25, 118)
+        Me.lblTeamNameLeft.Name = "lblTeamNameLeft"
+        Me.lblTeamNameLeft.Size = New System.Drawing.Size(412, 65)
+        Me.lblTeamNameLeft.TabIndex = 3
+        Me.lblTeamNameLeft.Text = "Left Team Name"
+        Me.lblTeamNameLeft.UseCompatibleTextRendering = True
+        '
+        'lblTeamLocLeft
+        '
+        Me.lblTeamLocLeft.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.lblTeamLocLeft.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer))
+        Me.lblTeamLocLeft.Font = New System.Drawing.Font("Roboto Slab", 33.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeamLocLeft.ForeColor = System.Drawing.Color.White
+        Me.lblTeamLocLeft.Location = New System.Drawing.Point(25, 53)
+        Me.lblTeamLocLeft.Name = "lblTeamLocLeft"
+        Me.lblTeamLocLeft.Size = New System.Drawing.Size(412, 65)
+        Me.lblTeamLocLeft.TabIndex = 2
+        Me.lblTeamLocLeft.Text = "Left Team City" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblTeamLocLeft.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        Me.lblTeamLocLeft.UseCompatibleTextRendering = True
+        '
+        'lblScoreLeft
+        '
+        Me.lblScoreLeft.Font = New System.Drawing.Font("Roboto Slab", 220.0!, System.Drawing.FontStyle.Bold)
+        Me.lblScoreLeft.ForeColor = System.Drawing.Color.White
+        Me.lblScoreLeft.Location = New System.Drawing.Point(-13, 284)
+        Me.lblScoreLeft.Name = "lblScoreLeft"
+        Me.lblScoreLeft.Size = New System.Drawing.Size(602, 416)
+        Me.lblScoreLeft.TabIndex = 4
+        Me.lblScoreLeft.Text = "768"
+        Me.lblScoreLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lblScoreLeft.UseCompatibleTextRendering = True
+        '
+        'lblScoreRight
+        '
+        Me.lblScoreRight.Font = New System.Drawing.Font("Roboto Slab", 220.0!, System.Drawing.FontStyle.Bold)
+        Me.lblScoreRight.ForeColor = System.Drawing.Color.White
+        Me.lblScoreRight.Location = New System.Drawing.Point(691, 284)
+        Me.lblScoreRight.Name = "lblScoreRight"
+        Me.lblScoreRight.Size = New System.Drawing.Size(602, 416)
+        Me.lblScoreRight.TabIndex = 5
+        Me.lblScoreRight.Text = "768"
+        Me.lblScoreRight.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.lblScoreRight.UseCompatibleTextRendering = True
+        '
+        'FadeTimer
+        '
+        Me.FadeTimer.Interval = 60
+        '
+        'Scoreboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImage = Global.JANIS.My.Resources.Resources.ScoreTemplate
+        Me.Controls.Add(Me.lblScoreRight)
+        Me.Controls.Add(Me.lblScoreLeft)
+        Me.Controls.Add(Me.lblTeamNameLeft)
+        Me.Controls.Add(Me.lblTeamLocLeft)
+        Me.Controls.Add(Me.lblTeamNameRight)
+        Me.Controls.Add(Me.lblTeamLocRight)
+        Me.Name = "Scoreboard"
+        Me.Size = New System.Drawing.Size(1280, 720)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblTeamLocRight As System.Windows.Forms.Label
+    Friend WithEvents lblTeamNameRight As System.Windows.Forms.Label
+    Friend WithEvents lblTeamNameLeft As System.Windows.Forms.Label
+    Friend WithEvents lblTeamLocLeft As System.Windows.Forms.Label
+    Friend WithEvents lblScoreLeft As System.Windows.Forms.Label
+    Friend WithEvents lblScoreRight As System.Windows.Forms.Label
+    Friend WithEvents FadeTimer As System.Windows.Forms.Timer
+
+End Class
---
+++ ScoreboardControl.vb
@@ -0,0 +1,107 @@
+﻿Public Class ScoreboardControl
+    Inherits System.Windows.Forms.UserControl
+
+    Dim LEFT_TEAM_COLOR As System.Drawing.Color = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer))
+    Dim RIGHT_TEAM_COLOR As System.Drawing.Color = System.Drawing.Color.Maroon
+    Dim DEFAULT_SCORE_COLOR As System.Drawing.Color = System.Drawing.Color.White
+    Dim LEFT_FADE_INCREMENTS() As Integer = {0, 16, 16, 5}     '* If any of these had to decrease to reach the goal color,
+    Dim RIGHT_FADE_INCREMENTS() As Integer = {0, 8, 16, 16}    '* The numbers would be negative, but that would break AddColor()
+    Const MAX_SCORE As Integer = 999
+    Const MIN_SCORE As Integer = -99
+
+
+    Private Sub Scoreboard_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function Limited_Score(ByVal score As Integer) As Integer
+        '* If the score is too big or too small, it will be too wide to display
+        Return Math.Min(Math.Max(score, MIN_SCORE), MAX_SCORE)
+    End Function
+
+    Private Function AddColor(ByVal color As System.Drawing.Color, ByVal increments() As Integer) As System.Drawing.Color
+        Dim a As Integer = Math.Min(increments(0) + color.A, DEFAULT_SCORE_COLOR.A)
+        Dim r As Integer = Math.Min(increments(1) + color.R, DEFAULT_SCORE_COLOR.R)
+        Dim g As Integer = Math.Min(increments(2) + color.G, DEFAULT_SCORE_COLOR.G)
+        Dim b As Integer = Math.Min(increments(3) + color.B, DEFAULT_SCORE_COLOR.B)
+
+        Return System.Drawing.Color.FromArgb(a, r, g, b)
+    End Function
+
+    Private Sub FadeBuff(ByRef lbl As System.Windows.Forms.Label, buffcolor As System.Drawing.Color)
+        lbl.ForeColor = buffcolor
+        If Not Me.FadeTimer.Enabled Then Me.FadeTimer.Start()
+    End Sub
+
+    Private Sub FadeTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FadeTimer.Tick
+        Dim changed As Boolean = False
+        If Not Me.lblScoreLeft.ForeColor.Equals(DEFAULT_SCORE_COLOR) Then
+            Me.lblScoreLeft.ForeColor = Me.AddColor(Me.lblScoreLeft.ForeColor, LEFT_FADE_INCREMENTS)
+            changed = True
+        End If
+        If Not Me.lblScoreRight.ForeColor.Equals(DEFAULT_SCORE_COLOR) Then
+            Me.lblScoreRight.ForeColor = Me.AddColor(Me.lblScoreRight.ForeColor, RIGHT_FADE_INCREMENTS)
+            changed = True
+        End If
+        If Not changed Then Me.FadeTimer.Stop()
+    End Sub
+
+#Region "Public Properties"
+    Public Property TeamLocLeft As String
+        Get
+            Return Me.lblTeamLocLeft.Text
+        End Get
+        Set(value As String)
+            Me.lblTeamLocLeft.Text = value
+        End Set
+    End Property
+    Public Property TeamLocRight As String
+        Get
+            Return Me.lblTeamLocRight.Text
+        End Get
+        Set(value As String)
+            Me.lblTeamLocRight.Text = value
+        End Set
+    End Property
+    Public Property TeamNameLeft As String
+        Get
+            Return Me.lblTeamNameLeft.Text
+        End Get
+        Set(value As String)
+            Me.lblTeamNameLeft.Text = value
+        End Set
+    End Property
+    Public Property TeamNameRight As String
+        Get
+            Return Me.lblTeamNameRight.Text
+        End Get
+        Set(value As String)
+            Me.lblTeamNameRight.Text = value
+        End Set
+    End Property
+    Public Property ScoreLeft As Integer
+        Get
+            Return Convert.ToInt32(Me.lblScoreLeft.Text)
+        End Get
+        Set(ByVal value As Integer)
+            Me.lblScoreLeft.Text = Limited_Score(value).ToString
+            FadeBuff(lblScoreLeft, LEFT_TEAM_COLOR)
+        End Set
+    End Property
+    Public Property ScoreRight As Integer
+        Get
+            Return Convert.ToInt32(Me.lblScoreRight.Text)
+        End Get
+        Set(ByVal value As Integer)
+            Me.lblScoreRight.Text = Limited_Score(value).ToString
+            FadeBuff(lblScoreRight, RIGHT_TEAM_COLOR)
+        End Set
+    End Property
+
+#End Region
+#Region "Public Methods"
+
+#End Region
+
+End Class
+
