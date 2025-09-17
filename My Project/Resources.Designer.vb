BrBill/janis

commit 1d1dafe7fc53b7f25f22fea6543ca2582c58caf5

Improve some UI elements, add tooltips, fix blank score causing exception, and adjusted color picker black to 'near black'

My Project/Resources.Designer.vb
@@ -173,6 +173,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property send_to_TV_cropped() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("send-to-TV-cropped", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property web() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("web", resourceCulture)

My Project/Resources.resx
@@ -121,23 +121,26 @@
   <data name="left-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\left-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
-  <data name="HotButton_Divided" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\HotButton_Divided.bmp;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
-  </data>
-  <data name="left-right-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\left-right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
-  </data>
-  <data name="harddrive" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\hard-drive.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  <data name="ScoreTemplate" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\ScoreTemplate.bmp;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
   <data name="RobotoSlab_Bold" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\RobotoSlab-Bold.ttf;System.Byte[], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </data>
+  <data name="harddrive" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\hard-drive.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="HotButton_Divided" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\HotButton_Divided.bmp;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
+  <data name="right-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
   <data name="cdrom" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\cd-rom.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
-  <data name="ScoreTemplate" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\ScoreTemplate.bmp;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  <data name="left-right-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\left-right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
   <data name="web" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\web.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
@@ -151,7 +154,7 @@
   <data name="lock" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\lock.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
-  <data name="right-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  <data name="send-to-TV-cropped" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\send-to-TV-cropped.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
 </root>

fmColorDialog.vb
@@ -207,7 +207,7 @@ Public Class fmColorDialog
         '
         'Panel6
         '
-        Me.Panel6.BackColor = System.Drawing.Color.Black
+        Me.Panel6.BackColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer), CType(CType(1, Byte), Integer))
         Me.Panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.Panel6.Location = New System.Drawing.Point(8, 128)
         Me.Panel6.Name = "Panel6"

fmMain.resx
@@ -132,12 +132,6 @@
   <metadata name="grpClearText.Locked" type="System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
     <value>True</value>
   </metadata>
-  <metadata name="grpLoadText.Locked" type="System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
-    <value>True</value>
-  </metadata>
-  <metadata name="grpClearText.Locked" type="System.Boolean, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
-    <value>True</value>
-  </metadata>
   <data name="TextBox2.Text" xml:space="preserve">
     <value>This program may only be used by ComedySportz, or by explicit permission of the author, Bill Cernansky.
 
@@ -154,14 +148,8 @@ CTRL-SHIFT-B reveals the truth.</value>
   <metadata name="SlideTimer.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
     <value>21, 2</value>
   </metadata>
-  <metadata name="ToolTip1.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
-    <value>374, 3</value>
-  </metadata>
   <metadata name="CountdownTimer.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
     <value>228, 3</value>
-  </metadata>
-  <metadata name="ToolTip1.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
-    <value>374, 3</value>
   </metadata>
   <metadata name="$this.TrayHeight" type="System.Int32, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089">
     <value>32</value>

fmScreen.vb
@@ -191,9 +191,9 @@ Public Class fmScreen
         '
         'fmScreen
         '
-        Me.AutoScaleBaseSize = New System.Drawing.Size(8, 19)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(1920, 1081)
+        Me.ClientSize = New System.Drawing.Size(1920, 1080)
         Me.Controls.Add(Me.lblTeamNameRight)
         Me.Controls.Add(Me.lblTeamLocRight)
         Me.Controls.Add(Me.lblScoreRight)
@@ -452,37 +452,9 @@ Public Class fmScreen
 
 
 #Region "Private Functions and Subs"
-    'Private Sub fmScreen_Resize(sender As Object, e As EventArgs) Handles Me.Resize
-    '    Dim scaleW As Double = Me.Width / WINDOW_BASE_WIDTH
-    '    Dim scaleH As Double = Me.Height / WINDOW_BASE_HEIGHT
-    '    MsgBox("Width: " + Me.Width.ToString + ", Height is " + Me.Height.ToString, MsgBoxStyle.OkOnly, "SizeInfo")
-
-    '    RescaleControl(lblScoreLeft, scaleW, scaleH)
-    '    RescaleControl(lblScoreRight, scaleW, scaleH)
-    '    RescaleControl(lblTeamNameLeft, scaleW, scaleH)
-    '    RescaleControl(lblTeamNameRight, scaleW, scaleH)
-    '    RescaleControl(lblTeamLocLeft, scaleW, scaleH)
-    '    RescaleControl(lblTeamLocRight, scaleW, scaleH)
-    '    RescaleControl(lblMsg, scaleW, scaleH)
-    '    RescaleControl(lblCountdown, scaleW, scaleH)
-    '    picGraphic.Width = Me.Width
-    '    picGraphic.Height = Me.Height
-    'End Sub
-
-    'Private Sub RescaleControl(ctrl As Control, scaleW As Double, scaleH As Double)
-    '    ctrl.Left = CInt(ctrl.Left * scaleW)
-    '    ctrl.Top = CInt(ctrl.Top * scaleH)
-    '    ctrl.Width = CInt(ctrl.Width * scaleW)
-    '    ctrl.Height = CInt(ctrl.Height * scaleH)
-    '    If TypeOf ctrl Is Label Or TypeOf ctrl Is gLabel.gLabel Then
-    '        Dim oldFont As Font = ctrl.Font
-    '        ctrl.Font = New Font(oldFont.FontFamily, Convert.ToSingle(oldFont.Size * Math.Min(scaleW, scaleH)), oldFont.Style)
-    '    End If
-    'End Sub
-
-
     Private Function Limited_Score(ByVal score As String) As Integer
         '* If the score is too big or too small, it will be too wide to display
+        If score = "" Then Return ""
         Return Math.Min(Math.Max(Convert.ToInt32(score), MIN_SCORE), MAX_SCORE).ToString
     End Function
 

commit 5d07be2fe650d541ad48e8799dcf1020b68aec5b

fix up broken Resources files corrupted in commit 1d1dafe

My Project/Resources.Designer.vb
@@ -170,16 +170,7 @@ Namespace My.Resources
             End Get
         End Property
         
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property send_to_TV_cropped() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("send-to-TV-cropped", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
+       
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>

My Project/Resources.resx
@@ -139,6 +139,9 @@
   <data name="cdrom" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\cd-rom.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
+  <data name="ScoreTemplate" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>..\Resources\left-right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
+  </data>
   <data name="left-right-arrow" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\left-right-arrow.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
@@ -153,8 +156,5 @@
   </data>
   <data name="lock" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>..\Resources\lock.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
-  </data>
-  <data name="send-to-TV-cropped" type="System.Resources.ResXFileRef, System.Windows.Forms">
-    <value>..\Resources\send-to-TV-cropped.png;System.Drawing.Bitmap, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a</value>
   </data>
 </root>